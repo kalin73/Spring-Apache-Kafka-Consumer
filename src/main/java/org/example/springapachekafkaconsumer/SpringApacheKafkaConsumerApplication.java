b26538882b13e328a815bd6364e0014b281c4c61
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-06-19 00:07:33 +0300

    init commit

---
+++ src/main/java/org/example/springapachekafkaconsumer/SpringApacheKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package org.example.springapachekafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApacheKafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApacheKafkaConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springapachekafkaconsumer/SpringApacheKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springapachekafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApacheKafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
